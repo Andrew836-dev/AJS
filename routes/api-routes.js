36b60c7982037ae3f1b37d99b4a685f9dcba2d4e
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-13 19:46:18 +1030

    added body parsing to login route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,7 +8,11 @@ module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), (req, res) => {
+  app.post("/api/login", [
+    body('email').trim().isEmail().normalizeEmail(),
+    body('name').trim().notEmpty().escape(),
+    body('password').isLength({ min: PASSWORD_MIN_LENGTH }), 
+    passport.authenticate("local")], (req, res) => {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
@@ -16,26 +20,26 @@ module.exports = function (app) {
     });
   });
 
-  // Route for signing up a user. The user data is validated and sanitized thanks to express-validator middleware.
-  // The user's password is automatically hashed and stored securely thanks to passport-local-mongoose middleware.
-  // If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", [
-    body('email').trim().isEmail().normalizeEmail(),
-    body('name').trim().notEmpty().escape(),
-    body('password').trim().isLength({ min: PASSWORD_MIN_LENGTH })
-  ], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const { name, email, password } = req.body;
-    controllers.registerNewUser(name, email, password)
-      .then((response) => {
-        return res.redirect(307, "/api/login")
-      })
-      .catch(err => res.status(401).json(err));
-  });
+    // Route for signing up a user. The user data is validated and sanitized thanks to express-validator middleware.
+    // The user's password is automatically hashed and stored securely thanks to passport-local-mongoose middleware.
+    // If the user is created successfully, proceed to log the user in,
+    // otherwise send back an error
+    app.post("/api/signup", [
+      body('email').trim().isEmail().normalizeEmail(),
+      body('name').trim().notEmpty().escape(),
+      body('password').isLength({ min: PASSWORD_MIN_LENGTH })
+    ], (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { name, email, password } = req.body;
+      controllers.registerNewUser(name, email, password)
+        .then((response) => {
+          return res.redirect(307, "/api/login")
+        })
+        .catch(err => res.status(401).json(err));
+    });
 
   // Route for logging user out
   app.get("/logout", (req, res) => {
