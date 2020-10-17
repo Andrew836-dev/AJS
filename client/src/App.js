5756c051d5fcded61b37676d98d78232c5fa7d60
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-17 15:09:20 +1030

    added routing for editor, login, landing and profile page. Added userContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+/**
+ * TODO: express-acl, EsPrima
+ * 
+ * https://uiwjs.github.io/react-codemirror/
+ * https://esprima.readthedocs.io/en/4.0/syntactic-analysis.html#distinguishing-a-script-and-a-module
+ */
+
+
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Box, Button, Heading, Grommet } from "grommet";
 import { Notification } from "grommet-icons";
+import { UserProvider } from "./utils/UserStore";
 import API from "./utils/API";
+import Landing from "./pages/Landing";
+import Profile from "./pages/Profile";
+import Editor from "./pages/Editor";
+import Login from "./pages/Login";
 
 const theme = {
   global: {
@@ -32,12 +46,30 @@ const AppBar = props => (
 
 function App() {
   return (
-    <Grommet theme={theme}>
-      <AppBar>
-        <Heading level="3" margin="none">My App</Heading>
-        <Button icon={<Notification />} onClick={() => { API.userLogin("Teddy@example.com", "teddypass") }} />
-      </AppBar>
-    </Grommet>
+    <UserProvider>
+      <Grommet theme={theme}>
+        <AppBar>
+          <Heading level="3" margin="none" onClick={() => API.userSignUp("Teddy", "Teddy@example.com", "teddypass")}>My App</Heading>
+          <Button icon={<Notification />} onClick={() => { API.userLogin("Teddy@example.com", "teddypass") }} />
+        </AppBar>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Landing />
+            </Route>
+            <Route exact path={["/code", "/code/:language"]}>
+              <Editor />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </Router>
+      </Grommet>
+    </UserProvider>
   );
 }
 
