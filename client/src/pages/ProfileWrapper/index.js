ee400ec6ecb802d06f79075d30149410fc456db7
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-20 10:10:19 +1030

    added profile page

---
+++ client/src/pages/ProfileWrapper/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useUserContext } from "../../utils/UserStore";
+import UserProfile from "../../components/UserProfile";
+
+function ProfileWrapper() {
+  const { userName } = useParams();
+  const [userContext, userDispatch] = useUserContext();
+
+  return (!userName
+    ? <UserProfile profileName={userContext.name} />
+    : <UserProfile profileName={userName} />)
+}
+
+export default ProfileWrapper;
