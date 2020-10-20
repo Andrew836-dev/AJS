3a1c7cf5eaa15f80891646119f6795f6e58cea99
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-20 19:30:37 +1030

    added find user function for profile data

--- controllers/index.js
+++ controllers/index.js
@@ -9,34 +9,41 @@ const defaultMongoOptions = {
 
 async function checkIfEmailInUse (email) {
   // returns Boolean
-  return db.User.findOne({ email }).then((dbUser) => !!dbUser);
+  return db.User.findOne({ email })
+    .then(dbUser => !!dbUser);
 }
 
 async function checkIfNameInUse (name) {
   // returns Boolean
-  return db.User.findOne({ name }).then((dbuser) => !!dbuser);
+  return db.User.findOne({ name })
+    .then(dbuser => !!dbuser);
+}
+
+async function getUserData (name) {
+  return db.User.findOne({ name });
 }
 
 async function registerNewUser (name, email, password) {
   return db.User.create({ name, email })
-    .then((newUser) => newUser.setPassword(password))
-    .then((saltedUser) => saltedUser.save())
-    .catch((err) => Promise.reject(err));
+    .then(newUser => newUser.setPassword(password))
+    .then(saltedUser => saltedUser.save())
+    .catch(err => Promise.reject(err));
 }
 
 async function updateLastLoginById (id) {
   return db.User.findById(id)
-    .then((dbUser) => dbUser.updateLoginAndSave())
-    .catch((err) => Promise.reject(err));
+    .then(dbUser => dbUser.updateLoginAndSave())
+    .catch(err => Promise.reject(err));
 }
 
 module.exports = {
+  checkIfEmailInUse,
+  checkIfNameInUse,
   connect: (uri = MONGODB_URI, options = defaultMongoOptions) =>
     mongoose.connect(uri, options),
   disconnect: () => mongoose.disconnect(),
   getHostString: () => mongoose.connection.host ? mongoose.connection.host : "Not connected",
+  getUserData,
   registerNewUser,
-  checkIfEmailInUse,
-  checkIfNameInUse,
   updateLastLoginById
 };
