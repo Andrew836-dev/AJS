8db1764ae9f5e9400129837bc2116092f2209e9d
Andrew Gray <andrew.aj.gray@gmail.com>
2020-11-01 21:32:22 +1030

    added information to landing page

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,20 +1,28 @@
 import React, { useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { Box, Button } from "grommet";
+import { Link, useHistory } from "react-router-dom";
+import { Box, Button, List } from "grommet";
 import { useUserContext } from "../../utils/UserStore";
 
 function Landing() {
   const [userState] = useUserContext();
   const history = useHistory();
   return <Box>
-    <Box direction="column" justify="center">
+    <Box direction="column" justify="center" width={{ max: "350px" }}>
+      <Box>
+        Welcome to AJS! A tool for Web Development students to help smooth the learning process.
+        <ul>
+          <li>Use the <Link to="/editor/javascript">JavaScript editor</Link> to parse JavaScript and see what each line of code represents.</li>
+          <li>Use the <Link to="/editor/markdown">Markdown editor</Link> to help write your README.md files.</li>
+          <li>If you choose to Sign in, you will be able to save code and share it with others.</li>
+        </ul>
+      </Box>
       {!userState.username && (
         <>
           <Button label="Login" onClick={() => history.push("/login")} />
           <Button label="Register" onClick={() => history.push("/register")} />
         </>
       )}
-      <Button label="Javascript Editor" onClick={() => history.push("/editor/javascript")} />
+      <Button label="JavaScript Editor" onClick={() => history.push("/editor/javascript")} />
       <Button label="Markdown Editor" onClick={() => history.push("/editor/markdown")} />
       {!!userState.username && (
         <>
